45f0d998c4ba796d50d0659f25a4b4fd6b49097e
jtrejo13 <jtrejo13@utexas.edu>
2017-12-22 10:59:34 -0600

    Implement multiply.py

--- arrays/multiply.py
+++ arrays/multiply.py
@@ -8,6 +8,7 @@ the function should return <-1,4,7,5,7,3,9,5,2,5,8,9,6,7,6,4,1,2,9,2,7>
 """
 
 
+# bute force might overflow
 def brute_mult(A, B):
     num_a = int(''.join([str(a) for a in A]))
     num_b = int(''.join([str(b) for b in B]))
@@ -19,19 +20,26 @@ def brute_mult(A, B):
 
 
 def multiply(A, B):
-    pass
+    # absolute value of A and B
+    sign = -1 if A[0] * B[0] < 0 else 1
+    A[0], B[0] = abs(A[0]), abs(B[0])
+
     # multiply [ai] * [bj]
     # carry over when == 10
-    # sum results
-    ans = []
-    count = 0
+    res = [0] * (len(A) + len(B))
     for i in reversed(range(len(A))):
-        num = []
         for j in reversed(range(len(B))):
-            mul = A[i] * B[j]
-            num.insert(0, mul)
-        ans.append(num)
-    print(ans)
+            res[i + j + 1] += A[i] * B[j]
+            res[i + j] += res[i + j + 1] // 10
+            res[i + j + 1] %= 10
+
+    # remove leading zeros
+    while res[0] == 0:
+        res.pop(0)
+    # update sign
+    res[0] = sign * res[0]
+
+    return res
 
 
 if __name__ == '__main__':
@@ -48,6 +56,10 @@ if __name__ == '__main__':
         else failed()
 
     passed() if multiply([2], [3]) == [6] else failed()
-    passed() if multiply([1, 2], [2]) == [2, 4] else failed()
-    passed() if multiply([1, 2], [2, 4]) == [2, 8, 8] else failed()
-    passed() if False else failed()
+    passed() if multiply([1, 2, 3], [-2]) == [-2, 4, 6] else failed()
+    passed() if multiply([1, 2, 3], [2, 2, 2]) == [2, 7, 3, 0, 6] \
+                               else failed()
+    passed() if multiply(
+        [1, 9, 3, 7, 0, 7, 7, 2, 1], [-7, 6, 1, 8, 3, 8, 2, 5, 7, 2, 8, 7]) \
+        == [-1, 4, 7, 5, 7, 3, 9, 5, 2, 5, 8, 9, 6, 7, 6, 4, 1, 2, 9, 2, 7] \
+        else failed()
